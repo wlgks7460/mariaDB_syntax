0e4a87c8fc58f0f514882c6253e743ef8ac77198
wlgks7460 <wlgks7460@naver.com>
2023-11-16 12:41:50 +0900

    gg

--- basic/1.DDL.sql
+++ basic/1.DDL.sql
@@ -5,3 +5,7 @@ USE board;
 -- 테이블 신규 생성
 CREATE TABLE author(id INT name VARCHAR(255),
  email VARCHAR(255), password VARCHAR(255), test1 VARCHAR(255), PRIMARY KEY (id));
+--  테이블 목록 조회
+SHOW TABLES;
+-- 테이블
+DESCRIBE author;
