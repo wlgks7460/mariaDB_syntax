9a826f4a7f1a7d235511873e259bfe9ffc11a481
wlgks7460 <wlgks7460@naver.com>
2023-11-16 12:39:35 +0900

    gg

---
+++ basic/1.DDL.sql
@@ -0,0 +1,7 @@
+-- 데이터베이스 생성
+CREATE DATABASE board;
+-- 데이터베이스 선택
+USE board;
+-- 테이블 신규 생성
+CREATE TABLE author(id INT name VARCHAR(255),
+ email VARCHAR(255), password VARCHAR(255), test1 VARCHAR(255), PRIMARY KEY (id));
